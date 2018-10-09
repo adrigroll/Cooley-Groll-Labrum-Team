6e712a9218dfff62fff7bbf3b0ff036278d6e7aa
your name <gro12003Gbyui.edu>
2018-10-08 20:34:36 -0600

    10/8/2018 file prologue

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -1,8 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Spring 2018
+ *  Team members: John Doe, Amy Brown, Abe Smith
  */
+
 package cityofaaron;
 
 /**
