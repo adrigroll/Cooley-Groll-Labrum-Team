a1c9b80ffcdcb1638f9ffbd3bfd98f79eff319a6
Ben <Ben@He-Man>
2018-10-15 19:40:49 -0600

    Week 4 Assignments

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -10,6 +10,8 @@ package cityofaaron;
 import model.*;
 public class CityOfAaron {
 
+    private static int dozen;
+
     /**
      * @param args the command line arguments
      */
@@ -36,11 +38,27 @@ public class CityOfAaron {
         cropData.setHarvest(3);
         cropData.setWheatForFood(4);
         cropData.setAcresPlanted(5);
+        cropData.setYear(6);
+        cropData.setPopulation(7);
+        cropData.setacresOwned(8);
+        cropData.setwheatInStore(9);
+        cropData.setcropYield(10);
         System.out.println(cropData.getNumberWhoDied());
         System.out.println(cropData.getNewPeople());
         System.out.println(cropData.getHarvest());
         System.out.println(cropData.getWheatForFood());
         System.out.println(cropData.getAcresPlanted());
+        System.out.println(cropData.getYear());
+        System.out.println(cropData.getPopulation());
+        System.out.println(cropData.getAcresOwned());
+        System.out.println(cropData.getWheatInStore());
+        System.out.println(cropData.getCropYield());        
+     
+        ListItem apple = new ListItem();
+        apple.setName("Granny Smith");
+        apple.setNumber(dozen);
+        System.out.println(apple.getName());
+        System.out.println(apple.getNumber());
         
     }
   
