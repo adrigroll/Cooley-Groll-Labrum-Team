1a98b4e5e15cbf0847e48b6986d6e90b2e187683
Siegel-Gavin <gjwsiegel@gmail.com>
2018-11-07 18:50:54 -0500

    Create ListMenuView.java

---
+++ cityOfAaron/src/view/ListMenuView.java
@@ -0,0 +1,52 @@
+package view;
+
+/*The ListMenuView class - part of the view layer
+ * Object: Manage the list menu
+ * @author Gavin Siegel
+ * Date last modified: February 2018
+ */
+import view.MainMenuView;
+import java.util.Scanner;
+import cityofaaron.CityOfAaron;
+
+
+public class ListMenuView{
+    
+    private String listMenu;
+    private int max;
+    Scanner keyboard = new Scanner(System.in);
+    
+    public ListMenuView(){
+   
+    }
+
+    public void displayMenuView(){
+
+    }
+    
+    public int getMenuOption(){
+        return 0;
+    }
+    
+
+    public void doAction(int option)
+    {
+
+    }   
+
+    public void listAnimals(){
+ 
+    }
+
+    public void listTools(){
+
+    }
+
+    public void listProvisions(){
+
+    }
+
+    public void listTeam(){
+ 
+    }
+}
