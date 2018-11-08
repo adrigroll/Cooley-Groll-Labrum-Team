774f96422ba6669670020c2ce361b6d006190409
adrigroll <43461438+adrigroll@users.noreply.github.com>
2018-11-07 22:47:39 -0700

    Update CropView.java

--- cityOfAaron/src/view/CropView.java
+++ cityOfAaron/src/view/CropView.java
@@ -17,4 +17,46 @@ import cityofaaron.CityOfAaron;
 
 public class CropView {
     
+    //ADRIENNE GROLL//
+    //The setOfferingView method
+    // Purpose: The method to allocate a percentage of the harvest to be paid as an offering
+    // Parameters: none
+    // Returns: none
+    //Stores: setOffering in the CropData object
+    
+    public void setOfferingView() 
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+
+    int offering = CropControl.setOffering();
+    
+    system.out.println("\nWhat percentage of the harvest do you want to pay as an offering?");
+            
+        //  Get the user’s input and save it.
+        int offering;
+        offering = keyboard.nextInt();
+        
+        CropControl.setOffering(offering, cropData);
+            } 
+    
+    // The buyLandView method
+    // Purpose: interface with the user input for buying land
+    // Parameters: none
+    // Returns: none
+    public static void buyLandView() {
+        // Get the cost of land for this round.
+        int price = CropControl.calcLandCost();
+
+        // Prompt the user to enter the number of acres to buy
+        System.out.format("Land is selling for %d bushels per acre.%n",price);
+        System.out.print("\nHow many acres of land do you wish to buy?");
+
+        //  Get the user’s input and save it.
+        int toBuy;
+        toBuy = keyboard.nextInt();
+        
+        CropControl.buyLand(price, toBuy, cropData);
+        }
+    
 }
